be2701dc01ddc92f91160ace73ede0c647f1a207
Lurldgbodex <professionaleditor6@gmail.com>
2025-02-08 20:31:10 +0100

    chore: configure cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
   const config = new DocumentBuilder()
