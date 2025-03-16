b95e8a1b622530bb6fb4a5ac649c2c574d45fddb
Lurldgbodex <professionaleditor6@gmail.com>
2025-03-16 19:15:29 +0100

    chore: update to enable navigation

--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -4,15 +4,17 @@ type SectionProps = {
     title: string;
     subTitle?: string;
     icon?: string;
+    route?: string;
 };
 
-const Section: React.FC<SectionProps> = ({ title, subTitle, icon }) => {
+
+const Section: React.FC<SectionProps> = ({ title, subTitle, icon, route }) => {
     return (
         <div className={`${styles.section} ${styles.className}`}>
             <h3>{title}</h3>
             {subTitle &&<div className={styles.sectionSub}>
                 {icon && <img src={icon} alt={subTitle} />}
-                <p>{subTitle}</p>
+                <a href={route}>{subTitle}</a>
             </div>}
         </div>
     )
